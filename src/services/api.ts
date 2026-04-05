c7ed62e47440e147bdb474cdc2a39aad6549a6d1
saliou <seyeserignesaliou36@gmail.com>
2026-04-05 13:14:34 +0000

    Fix: remove tsc from build script for Vercel

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api';
+// Utiliser la variable d'environnement
+const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api';
 
 const api = axios.create({
   baseURL: API_URL,
